7d9d26f0286a7329d4bffb40653eb319df5c7500
Sasha Shirokov <aliaksandrs@google.com>
2019-11-23 23:50:30 +0300

    added json output

--- client_server.py
+++ client_server.py
@@ -1,4 +1,5 @@
 from remote import Remote as r
+import json
 
 
 class MyServer(object):
@@ -17,3 +18,22 @@ s = MyServer('hosts.txt', 'passwords.txt')
 
 with open('result.txt', 'w') as f:
     f.write(s.server.execute())
+
+
+with open('result.txt', 'r') as f:
+    res = []
+    res_json = {}
+    a = f.read().split('\n')
+    for i, l in enumerate(a):
+        if i == 4 or i == len(a) - 2:
+            res.append(l)
+    for i, l in enumerate(res):
+        d = l.split()
+        if i == 0:
+            res_json['serverIP'] = d[3]
+            res_json['clientIP'] = d[8]
+        else:
+            res_json['Interval'] = d[2]
+            res_json['Transfer'] = d[4]
+            res_json['Bandwidth'] = d[6]
+    print(res_json)
